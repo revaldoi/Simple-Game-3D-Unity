1fddf916d990e5b5095f953c7b371fea23149286
Revaldo <revaldopratamaputra@Revaldos-MacBook-Pro.local>
2021-01-06 01:48:00 +0700

    latihan unity

---
+++ Assets/GerakanPlayer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GerakanPlayer : MonoBehaviour
+{
+    public float kecepatan;
+    Rigidbody rb;
+    Animator anim;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start() 
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Bergerak();
+    }
+
+    void Bergerak()
+    {
+        float gerak = Input.GetAxis("Horizontal");
+        rb.velocity = Vector3.right * gerak * kecepatan;
+        anim.SetFloat("Kecepatan", Mathf.Abs(gerak), 0.1f, Time.deltaTime);
+    }  
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("virus"))
+        {
+            Time.timeScale = 0;
+            Destroy(gameObject);
+        }
+    } 
+}
---
+++ Assets/MunculinVirus.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MunculinVirus : MonoBehaviour
+{
+    public GameObject virus;
+    public float waktuMin, waktuMaksimal;
+    public float posisiMinimal, posisiMaksimal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(MunculVirus());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator MunculVirus()
+    {
+        Instantiate(virus, transform.position + Vector3.right * Random.Range(posisiMinimal,posisiMaksimal), Quaternion.identity);
+        yield return new WaitForSeconds(Random.Range(waktuMin,waktuMaksimal));
+        StartCoroutine(MunculVirus());
+    }
+
+
+}
